fb275e426becd998d62808762b44a04ec37cd2c3
unknown <anderson.smachado@al.infnet.edu.br>
2022-09-21 00:14:08 -0300

    Criacao de 1 rota teste para crud e inicio de componente de login

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <NavBar/>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/crudteste">Teste de Crud</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/components/FormLogin.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="login">
+      <form>
+        <label for="nome">Seu nick</label>
+        <input
+            type = "text"
+            class = "form-control"
+            id = "nome"
+            placeholder="Digite aqui seu nome"
+        />
+        <label for="password">Senha:</label>
+        <input 
+            type="password" 
+            class = "form-control"
+            name="password" 
+            placeholder="Digite aqui seu nome"
+            id=""
+        >
+      </form>
+      <button type="submit" class="btn btn-primary">Entrar</button>
+    </div>
+  </template>
+
+<script>
+//   import NavBar from './components/NavBar.vue'
+
+export default {
+    name: 'FormLogin'
+    // components: {
+    //   NavBar
+    // }
+    }
+</script>
