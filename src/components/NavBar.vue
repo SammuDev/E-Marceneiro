ef473a5b3ceea4b460ba3add302a3327c6e26a50
unknown <anderson.smachado@al.infnet.edu.br>
2022-09-20 23:33:46 -0300

    Reiniciando projeto com Vue3 + Bootstrap + Vuex + Router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="navbar2">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container">
+              <a class="navbar-brand" href="">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                  <ul class="navbar-nav ms-auto">
+                      <li class="nav-item">
+                          <a class="nav-link" href="">Home</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="">Orçamentos</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="">Clientes</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="">Insumos</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="">Login</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="">Ajuda</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'NavBar',
+      props: {
+        msg: String
+      }
+    }
+</script>
+    
