b92a4bacee80705d981acd13a6abb317859b4998
JorgeInfnet <99371664+JorgeInfnet@users.noreply.github.com>
2022-09-22 09:10:27 -0300

    ajuste componente emailtool
    ajuste componente emailtool

---
+++ src/components/EmailTool2.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class="mb-3">
+    <label for="idEmailTool2" class="form-label">Email</label>
+           
+        <input
+            id="idEmailTool2"
+            type="email"
+            class="form-control"
+            :placeholder="placeHol"
+            :aria-label="placeHol"
+        >
+            
+   
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        placeHol:{}
+    }
+
+};
+</script>
+
+<style>
+</style>
--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,10 +1,11 @@
 <template>
 
-    <div class="container-fluid">
+    <div class="container-fluid px-2 py-3 px-sm-5 ">
         <h1 class="jumbotron ">Clientes</h1>
         <form class="p-3 border bg-light shadow-sm rounded " action="submit">
             <TextTool label="Nome" placeHol="Entre com nome do Cliente" />
-            <EmailTool/>
+            <EmailTool2 placeHol="Email do Cliente"/>
+
         </form>
     </div>
 
@@ -15,13 +16,14 @@
 <script>
 
 import TextTool from '@/components/TextTool.vue';
-import EmailTool from '@/components/EmailTool.vue';
+import EmailTool2 from '@/components/EmailTool2.vue';
 
 
 export default {
     components: {
         TextTool,
-        EmailTool
+        
+        EmailTool2
     }
 
 }
