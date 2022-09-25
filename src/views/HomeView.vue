0cbff18d68114a853e322d4fd71b54a859e9d235
JorgeInfnet <99371664+JorgeInfnet@users.noreply.github.com>
2022-09-25 00:42:08 -0300

    Ajusta pagina home view
    Ajustes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,9 @@
               <img src="../assets/brand\regua.webp" class="img-fluid" alt="Responsive image">
             </div>
         </div> -->
-      <div class="card-group ">
+      <div class="card-group mt-4">
         <div class="card">
-          <img class="card-img-top" src="../assets/brand\regua.webp" alt="Imagem de capa do card">
+          <img class="card-img-top" src="../assets/brand\regua.jpg" alt="Imagem de capa do card">
           <!-- <div class="card-body">
       <h5 class="card-title">Título do card</h5>
       <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional. Este conteúdo é um pouco maior, para demonstração.</p>
@@ -20,28 +20,24 @@
         <div class="card">
           <img class="card-img-top" src="../assets/brand/woodworker.jpg" alt="Imagem de capa do card">
           <div class="card-body">
-            <h5 class="card-title">Título do card</h5>
-            <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um
-              conteúdo adicional. Este conteúdo é um pouco maior, para demonstração.</p>
+            <h5 class="card-title">Lorem ipsum dolor sit amet.</h5>
+            <p class="card-text">Ut temporibus fuga qui cupiditate corporis qui tempora facere et doloribus accusamus eos enim culpa. Quo dolorem quis ab deserunt nihil ut magni porro et dicta minus est amet eius ea autem voluptatum. Et deleniti ratione ut neque possimus qui itaque magni quo quisquam nesciunt. Vel error consequatur et neque eveniet est galisum saepe ut blanditiis rerum.</p>
             <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
           </div>
         </div>
         <div class="card">
           <img class="card-img-top" src="../assets/brand/Carpinteiro.jpg" alt="Imagem de capa do card">
           <div class="card-body">
-            <h5 class="card-title">Título do card</h5>
-            <p class="card-text">Este é um card com suporte a texto embaixo, que funciona como uma introdução a um
-              conteúdo adicional.</p>
+            <h5 class="card-title">Lorem ipsum dolor sit amet.</h5>
+            <p class="card-text">Qui consequatur voluptatem qui consequatur mollitia quo galisum vero est tenetur ratione aut rerum omnis dolor expedita. Qui voluptatem voluptas hic neque galisum id quam aperiam qui deleniti tempore est galisum commodi qui ullam praesentium..</p>
             <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
           </div>
         </div>
         <div class="card">
           <img class="card-img-top" src="../assets/brand/martelo.jpg" alt="Imagem de capa do card">
           <div class="card-body">
-            <h5 class="card-title">Título do card</h5>
-            <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um
-              conteúdo adicional. Este card tem o conteúdo ainda maior que o primeiro, para mostrar a altura igual, em
-              ação.</p>
+            <h5 class="card-title">Lorem ipsum dolor sit amet.</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet. Et optio porro et saepe consectetur eum alias odit non blanditiis qui distinctio nesciunt. A libero quasi aut nihil repellendus et voluptas quos 33 galisum incidunt et rerum natus. Aut necessitatibus quia ut velit aliquid sed excepturi modi quo commodi quam hic perspiciatis culpa aut excepturi ipsa. Qui quibusdam dolores et corporis commodi vel reiciendis veritatis est sequi veritatis.</p>
             <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
           </div>
         </div>
