f21b072ab290397ebcb5e9535f43978fa62fbfc4
moraesleonardo <98929403+moraesleonardo@users.noreply.github.com>
2022-09-25 12:07:08 -0300

    Update OrcView.vue
    Iniciada a página de Orçamentos

--- src/views/OrcView.vue
+++ src/views/OrcView.vue
@@ -1,3 +1,92 @@
 <template>
-    <h1 class="jumbotron"> Pagina Orçamentos </h1>
+
+<section class="container">
+
+<div class="row margin">
+<div class="col-md-8 col-sm-0">
+<h3 class="jumbotron">Orçamento:</h3>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Data</th>
+            <th scope="col">-</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">0001</th>
+            <td>Mark</td>
+            <td>21/08/2022</td>
+            <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill"
+                viewBox="0 0 16 16">
+                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+            </svg></td>
+        </tr>
+        <tr>
+            <th scope="row">0002</th>
+            <td>Jacob</td>
+            <td>05/07/2022</td>
+            <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill"
+                viewBox="0 0 16 16">
+                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+            </svg></td>
+        </tr>
+        <tr>
+            <th scope="row">0003</th>
+            <td>Larry</td>
+            <td>25/06/2022</td>
+            <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill"
+                viewBox="0 0 16 16">
+                <path
+                    d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+            </svg></td>
+        </tr>
+    </tbody>
+</table>
+</div>
+<div class="col-md-4 col-sm-12">
+<h3 class="jumbotron">Dicas:</h3>
+<div class="margin">
+<p>Como cadastrar um novo orçamento?</p>
+<hr>
+<p>Como remover um novo orçamento?</p>
+<hr>
+<p>Outros tópicos</p>
+</div>
+</div>
+</div>
+<nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+        <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only"></span>
+            </a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only"></span>
+            </a>
+        </li>
+    </ul>
+</nav>
+</section>
 </template>
+
+<style scoped>
+.table{
+margin: 20px auto 400px auto
+;}
+.margin{
+margin: 50px auto auto auto;
+vertical-align: middle;
+}
+</style>
+
+   
