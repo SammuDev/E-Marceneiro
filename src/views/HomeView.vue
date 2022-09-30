a6f9a95b21ea9d5f3f608b5af0db614644c29d1d
unknown <anderson.smachado@al.infnet.edu.br>
2022-09-29 23:02:19 -0300

    ajustes nos conteudos dos cards, pagina principal. Falta colocar imagens corretas com o contexto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,25 +20,28 @@
         <div class="card">
           <img class="card-img-top" src="../assets/brand/woodworker.jpg" alt="Imagem de capa do card">
           <div class="card-body">
-            <h5 class="card-title">Lorem ipsum dolor sit amet.</h5>
-            <p class="card-text">Ut temporibus fuga qui cupiditate corporis qui tempora facere et doloribus accusamus eos enim culpa. Quo dolorem quis ab deserunt nihil ut magni porro et dicta minus est amet eius ea autem voluptatum. Et deleniti ratione ut neque possimus qui itaque magni quo quisquam nesciunt. Vel error consequatur et neque eveniet est galisum saepe ut blanditiis rerum.</p>
-            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+            <h5 class="card-title">Meus Orçamentos</h5>
+            <p class="card-text">Veja e gerencie todos os seus orçamentos desde o início até a entrega efetiva do contratado.</p>
+            <!-- <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p> -->
+            <router-link class="btn btn-primary" to="/orcview">Orçamentos</router-link>
           </div>
         </div>
         <div class="card  mx-3">
           <img class="card-img-top" src="../assets/brand/Carpinteiro.jpg" alt="Imagem de capa do card">
           <div class="card-body">
-            <h5 class="card-title">Lorem ipsum dolor sit amet.</h5>
-            <p class="card-text">Qui consequatur voluptatem qui consequatur mollitia quo galisum vero est tenetur ratione aut rerum omnis dolor expedita. Qui voluptatem voluptas hic neque galisum id quam aperiam qui deleniti tempore est galisum commodi qui ullam praesentium..</p>
-            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+            <h5 class="card-title">Meus Clientes</h5>
+            <p class="card-text">Cadastre, edite, comunique-se e gerencie todos os seus clientes de um único lugar!</p>
+            <!-- <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p> -->
+            <router-link class="btn btn-primary" to="/clientsview">Cliente</router-link>
           </div>
         </div>
         <div class="card">
           <img class="card-img-top" src="../assets/brand/martelo.jpg" alt="Imagem de capa do card">
           <div class="card-body">
-            <h5 class="card-title">Lorem ipsum dolor sit amet.</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet. Et optio porro et saepe consectetur eum alias odit non blanditiis qui distinctio nesciunt. A libero quasi aut nihil repellendus et voluptas quos 33 galisum incidunt et rerum natus. Aut necessitatibus quia ut velit aliquid sed excepturi modi quo commodi quam hic perspiciatis culpa aut excepturi ipsa. Qui quibusdam dolores et corporis commodi vel reiciendis veritatis est sequi veritatis.</p>
-            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+            <h5 class="card-title">Lista de Produtos/Insumos</h5>
+            <p class="card-text">Pesquise produtos e insumos para seus orçamentos nas lojas filiadas e sempre com os melhores preços para você!</p>
+            <p class="card-text"><small class="text-muted">{{qtdInsumos}} itens cadastrados</small></p>
+            <router-link class="btn btn-primary" to="/productview">Produtos e Insumos</router-link>
           </div>
         </div>
       </div>
@@ -53,7 +56,12 @@ export default {
   name: 'HomeView',
   components: {
 
-  }
+  },
+    computed: {
+        qtdInsumos() {
+            return this.$store.state.produtos.lista.length;
+        }
+    }
 }
 </script>
 
